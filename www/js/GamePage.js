7896d91ba96ed6f9ee3e06bf93ee831e09cbdf86
Zhenya <demichka@gmail.com>
2018-12-03 10:07:04 +0100

    created navbar wth links, added templates
    added templates to highscore and game pages

---
+++ www/js/GamePage.js
@@ -0,0 +1,6 @@
+class GamePage extends Component {
+    constructor() {
+        super();
+        this.addRoute('/our-game', 'Vårt spel');
+    }
+}
---
+++ www/js/HighscorePage.js
@@ -0,0 +1,7 @@
+class HighscorePage extends Component {
+    constructor() {
+        super();
+        this.addRoute('/highscore', 'Highscore');
+
+    }
+}
