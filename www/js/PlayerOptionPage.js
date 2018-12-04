1f3c71e1bc0e3fe274800d3075354fe5b1f5d844
Zhenya <demichka@gmail.com>
2018-12-04 14:30:26 +0100

    added function getPlayers()

--- www/js/PlayerOptionPage.js
+++ www/js/PlayerOptionPage.js
@@ -8,7 +8,24 @@ class PlayerOptionPage extends Component {
     }
 
     getPlayers() {
-        let name = this.baseEl.find('#nameFirstPlayer').val();
-        let type = this.baseEl.find('#typeFirstPlayer').val();
+        let nameOne = $('#nameFirstPlayer').val();
+        let typeOne = $('#typeFirstPlayer').val();
+        let nameTwo = $('#nameSecondPlayer').val();
+        let typeTwo = $('#typeSecondPlayer').val();
+        this.players.length = 0;
+        let playerOne = new Player(nameOne);
+        let playerTwo = new Player(nameTwo);
+        playerOne.human = true ? typeOne ==='human' : false;
+        playerTwo.human = true ? typeTwo ==='human' : false;
+        let p = $('<span class="badge badge-danger">Välj typ!</span>');
+        if (typeOne === 'fake') {
+            $('#typeFirstPlayer').parents('.input-group').after(p);
+        }
+        else if (typeTwo === 'fake') {
+            $('#typeSecondPlayer').parent('.input-group').append(p);
+        }
+        this.players.push(playerOne, playerTwo);
+        console.log(this.players);
+        console.log('clicked');
     }
 }
