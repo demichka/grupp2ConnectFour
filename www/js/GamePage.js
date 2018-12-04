bdde8a7e1f5ca9a819b2232d455262940759fe84
Mirwais Nezami <mirwais79@hotmail.com>
2018-12-04 12:13:20 +0100

    Set up of the grid for the game

--- www/js/GamePage.js
+++ www/js/GamePage.js
@@ -1,6 +1,38 @@
 class GamePage extends Component {
-    constructor() {
+    constructor(selector) {
         super();
         this.addRoute('/our-game', 'Vårt spel');
+        this.ROWS = 6;
+        this.COLS = 7;
+        this.selector = selector;
+        this.createGrid();
+    }
+
+    createGrid(){
+     const $board = $(this.selector);
+     //  console.log($board);
+     for (let row = 0; row < this.ROWS; row++){
+        const $row = $('<div>')
+        .addClass('row');
+
+        for (let col = 0; col < this.COLS; col++){
+            const $col = $('<div>')
+            .addClass('col empty');
+            $row.append($col);
+        }
+        $board.append($row);
+     }
+    // console.log($board.html());
     }
 }
+
+
+
+
+
+// Draw a grid
+//const connect4 = new GamePage('#connect4')
+$(document).ready(function(){
+   // const connect4 = new connect4('#connect4')
+    const connect4 = new GamePage('#connect4')
+});
