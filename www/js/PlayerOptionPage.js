d64b0a607eb74792e935f171c39282031042770f
Zhenya <demichka@gmail.com>
2018-12-05 09:58:34 +0100

    almost done

--- www/js/PlayerOptionPage.js
+++ www/js/PlayerOptionPage.js
@@ -1,31 +1,57 @@
 class PlayerOptionPage extends Component {
-    constructor() {
-        super();
-        this.players = [];
-        this.addEvents({
-            'click .startGameButton': 'getPlayers'
-        });
-    }
+  constructor() {
+    super();
+    this.players = [];
+    this.addEvents({
+      'click .startGameButton': 'getPlayers'
+    });
+  }
 
-    getPlayers() {
-        let nameOne = $('#nameFirstPlayer').val();
-        let typeOne = $('#typeFirstPlayer').val();
-        let nameTwo = $('#nameSecondPlayer').val();
-        let typeTwo = $('#typeSecondPlayer').val();
-        this.players.length = 0;
+  getPlayers() {
+    $('.badge').remove();
+
+    let p = '';
+    let nameOne = $('#nameFirstPlayer').val();
+    let typeOne = $('#typeFirstPlayer');
+    let nameTwo = $('#nameSecondPlayer').val();
+    let typeTwo = $('#typeSecondPlayer');
+    this.players.length = 0;
+    let validateType = (player) => { return player !== 'fake' ? true : false;};
+    let validateName = (name) => {return name.length > 2 && name.length <= 10 ? true : false;};
+
+    if( validateName(nameOne) &&
+        validateName(nameTwo) &&
+        validateType(typeOne.val()) &&
+        validateType(typeTwo.val())
+      ) {
+
+        $('.badge').remove();
         let playerOne = new Player(nameOne);
         let playerTwo = new Player(nameTwo);
-        playerOne.human = true ? typeOne ==='human' : false;
-        playerTwo.human = true ? typeTwo ==='human' : false;
-        let p = $('<span class="badge badge-danger">Välj typ!</span>');
-        if (typeOne === 'fake') {
-            $('#typeFirstPlayer').parents('.input-group').after(p);
-        }
-        else if (typeTwo === 'fake') {
-            $('#typeSecondPlayer').parent('.input-group').append(p);
-        }
+        playerOne.human = true ? typeOne.val() === 'human' : false;
+        playerTwo.human = true ? typeTwo.val() === 'human' : false;
         this.players.push(playerOne, playerTwo);
-        console.log(this.players);
-        console.log('clicked');
-    }
+      }
+    else {
+      if(!validateName(nameOne)) {
+        p = $('<span class="badge badge-danger">Namn måste innehålla mer än 2 och mindre 10 symboler</span>');
+        p.insertAfter(typeOne.parents('.input-group'));
+      }
+      if(!validateName(nameTwo)) {
+        p = $('<span class="badge badge-danger">Namn måste innehålla mer än 2 och mindre 10 symboler</span>');
+        p.insertAfter(typeTwo.parents('.input-group'));
+      }
+      if (!validateType(typeOne.val())) {
+        p = $('<span class="badge badge-danger">Välj typ!</span>');
+        p.insertAfter(typeOne);
+      } 
+      if (!validateType(typeTwo.val())) {
+        p = $('<span class="badge badge-danger">Välj typ!</span>');
+        p.insertAfter(typeTwo);
+        // typeTwo.after(p);
+      }
+    } 
+    console.log(this.players);
+    console.log('clicked');
+  }
 }
