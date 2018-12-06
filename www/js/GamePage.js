69677ae1ec78362e5329f2322ab101b333890890
Mirwais Nezami <mirwais79@hotmail.com>
2018-12-06 14:28:42 +0100

    gameboard bug fix

--- www/js/GamePage.js
+++ www/js/GamePage.js
@@ -1,52 +1,54 @@
 class GamePage extends Component {
-  constructor() {
-    super();
-    this.addRoute('/our-game', 'Vårt spel');
-    this.slots = [];
-    this.playerOptionPage = new PlayerOptionPage();
+    constructor() {
+        super();
+        this.addRoute('/our-game', 'Vårt spel');
+        this.playerOptionPage = new PlayerOptionPage();
 
-    this.addEvents({
-      // 'click .startGameButton': 'createGrid',
-      'click .abortGameButton': 'restartGame',
-      'click .startGameButton': 'getPlayers'      
-    });
-    this.createGrid();
-    this.restartGame();
+        this.addEvents({
+            // 'click .startGameButton': 'createGrid',
+            'click .abortGameButton': 'restartGame',
+            'click .startGameButton': 'getPlayers'
+        });
+        this.createGrid();
+        this.restartGame();
 
-    $('.abortGameButton').hide();
-    $('.nameInput').show();
-    $('.startGameButton').show();
-    $('.game-board').hide();
-  }
-
-
-  getPlayers() {
-    if (this.playerOptionPage.getPlayers()) {
-      this.createGrid();
+        $('.abortGameButton').hide();
+        $('.nameInput').show();
+        $('.startGameButton').show();
+        $('.game-board').hide();
     }
-  }
-  createGrid() {
-    $('.nameInput').hide();
-    $('.abortGameButton').show();
-    $('.startGameButton').hide();
-    
-    $('.game-board').show();  
-    // this.playersOptionPage.getPlayers();
-    for (let row = 0; row < 6; row++){
-        for (let col = 0; col < 7; col++) {
-          this.slots.push(new Slots (row, col));
+
+
+    getPlayers() {
+        if (this.playerOptionPage.getPlayers()) {
+            this.createGrid();
         }
     }
-  }
-  restartGame(){
-    $('.game-board').hide();  
+    createGrid() {
+        $('.nameInput').hide();
+        $('.abortGameButton').show();
+        $('.startGameButton').hide();
+
+        $('.game-board').show();
+        // this.playersOptionPage.getPlayers();
+        this.slots = [];
+        for (let row = 0; row < 6; row++) {
+            let rowArr = [];
+            for (let col = 0; col < 7; col++) {
+                rowArr.push(new Slots(this, row, col));
+                //this.slots.push(new Slots (row, col));
+            }
+            this.slots.push(rowArr);
+        }
+    }
+    columnClicked(col) {
+        alert('you clicked column' + col);
+    }
+
+restartGame() {
+    $('.game-board').hide();
     $('.nameInput').show();
     $('.abortGameButton').hide();
     $('.startGameButton').show();
-    }
 }
-
-
-
-
-
+}
